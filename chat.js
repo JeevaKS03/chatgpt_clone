cb4839f1217fc62c00e0be0461d9da50aeef8209
JEEVA K S <145172065+JeevaKS03@users.noreply.github.com>
2025-10-22 21:53:21 +0530

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,15 @@
+function handlesubmit(){
+    const inp_ele = document.getElementById("input");
+    let input = inp_ele.value;
+    inp_ele.value="";
+    const content_ele = document.getElementById("content");
+    content_ele.innerHTML+=`<br><br><h4>User:</h4>${input}`;
+    setTimeout(function(){
+        puter.ai.chat(input,{
+        model: 'gpt-5-nano',
+        }).then((data)=>{
+        console.log(data.message.content)
+        content_ele.innerHTML+=`<br><br><h4>ChatGPT:</h4>${data.message.content}`;
+        });
+    },1000)
+}
